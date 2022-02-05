519ef862a3d6667fedcf82ed70d0ef26bca5d754
rodrigogaleano <rodrigo1galeano@gmail.com>
2022-02-04 21:22:49 -0400

    Ajustes de design

--- script.js
+++ script.js
@@ -9,21 +9,38 @@ var tempo = 1000; //A variável tempo armazena um segundo (1000milisegundos)
 var cronometro; //Responsável pelo cronômetro
 var rodando = false; //Essa variável armazena se o cronometro está rodando ou não, começa em false
 
+//Botões
+var btn_iniciar = document.getElementById('iniciar');
+var btn_pausar = document.getElementById('pausar');
+var btn_zerar = document.getElementById('zerar');
+
 // ---------- FUNCÕES ----------
 
 function iniciar() { //Play
-    if (rodando == false){ //Verifica se o crônometro está em funcionamento para saber se é necessário startar
+    if (rodando == false) { //Verifica se o crônometro está em funcionamento para saber se é necessário startar
         rodando = true; //Armazena que o cronômetro está rodando
         cronometro = setInterval(() => timer(), tempo); //Chama a função timer
-    }    
+
+        //Estilo dos botões
+        btn_iniciar.style = 'transform: scale(1.1);'
+        btn_pausar.style = 'transform: scale(1.0);'
+        btn_zerar.style = 'color: linear-gradient(to right, #38ef7d, #11998e);'
+    }
 }
 
 function pausar() { //Pause
-    clearInterval(cronometro); //Pausa o cronômetro
-    rodando = false; //Armazena que o cronômetro não está rodando
+    if (rodando == true) {
+        clearInterval(cronometro); //Pausa o cronômetro
+        rodando = false; //Armazena que o cronômetro não está rodando
+
+        //Estilo dos botões
+        btn_iniciar.style = 'transform: scale(1.0);'
+        btn_pausar.style = 'transform: scale(1.1);'
+        btn_zerar.style = 'color: linear-gradient(to right, #38ef7d, #11998e);'
+    }
 }
 
-function parar() { //Stop
+function zerar() { //Stop
     rodando = false; //Armazena que o cronômetro não está rodando
     clearInterval(cronometro); //Pausa o cronômetro
 
@@ -32,20 +49,25 @@ function parar() { //Stop
     minutos = 0;
     segundos = 0;
 
+    //Estilo dos botões
+    btn_iniciar.style = 'transform: scale(1.0);'
+    btn_pausar.style = 'transform: scale(1.0);'
+    btn_zerar.style = 'background: linear-gradient(to right, #f85032, #e73827);'
+
     document.getElementById('relogio').innerText = '00:00:00'; //Altera o display do relógio para o padrão
 }
 
 function timer() { //Timer
     segundos++; //Incrementa os segundos a cada vez que entra na função
-    if (segundos == 60){ // Adiciona minuto
+    if (segundos == 60) { // Adiciona minuto
         segundos = 0;
         minutos++
-        if (minutos == 60){ // Adiciona hora
+        if (minutos == 60) { // Adiciona hora
             minutos = 0;
             hora++;
         }
     }
-  
+
     //Retorna o display formatado
     var display = (hora < 10 ? '0' + hora : hora) + ':' + (minutos < 10 ? '0' + minutos : minutos) + ':' + (segundos < 10 ? '0' + segundos : segundos)
     document.getElementById('relogio').innerText = display
